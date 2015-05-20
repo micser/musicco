edc08b34e62b327fc6be44579937863a1d00a1d0
musicco <musicco@e6b98db7-fc27-4867-be7c-257f7e29ac04>
2015-05-20 19:40:34 +0000

    WIP: #57 refreshed default theme - More finer changes around favicon, about box, panel sizes and icon quality.

--- trunk/index.php
+++ trunk/index.php
@@ -490,8 +490,24 @@ class Musicco
 <meta http-equiv="Content-Type" content="text/html; charset=<?php print $this->getConfig('charset'); ?>">
 <?php css(); ?>
 <!-- <meta charset="<?php print $this->getConfig('charset'); ?>" /> -->
-<link rel="icon" type="image/ico" href="favicon.ico">
-<link rel="apple-touch-icon" type="image/png" href="apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="manifest.json">
+<meta name="msapplication-TileColor" content="#a7a97f">
+<meta name="msapplication-TileImage" content="mstile-144x144.png">
+<meta name="theme-color" content="#a7a97f">
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="lib/jquery.jplayer.min.js"></script>
 <script type="text/javascript" src="lib/jplayer.playlist.min.js"></script>
@@ -2233,7 +2249,7 @@ function builddb() {
     function getAbout() {
    	$aboutString="<div id='aboutBox'>";
    	$aboutString.="<span class='close help'>&#10006;</span>";
-   	$aboutString.="<span class='about bold'><a target='_blank' href='//sf.net/projects/musicco/'><img src='apple-touch-icon.png'/></a></span>";
+   	$aboutString.="<span class='about bold'><a target='_blank' href='//sf.net/projects/musicco/'><img width='310px' height='310px' src='skins/".Musicco::getConfig('skin')."/about.png'/></a></span>";
    	$aboutString.="<span class='about bold'><a target='_blank' href='//sf.net/projects/musicco/'>musicco</a></span>";
    	$aboutString.="<span class='about bold'>A web based player for your music collection</span>";
    	$aboutString.="<span class='about bold'>and its android companion.</span>";
