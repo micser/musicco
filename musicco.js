f7ff747da7c12db6e29effe51c82a1f36318a737
micser <micser.micser@gmail.com>
2020-11-21 00:25:07 +0100

    WIP #2: Clean up service worker code

--- musicco.js
+++ musicco.js
@@ -1,36 +1,21 @@
-
-// What should you do??
-// - cache static assets
-// - cache covers from the playlist ==> probably not, the browser can do that
-// - cache current album songs
-// - handle library refresh, tell the page that a refresh is already ongoing, tell the page that a refresh has finished
-// - messaging: https://developer.mozilla.org/en-US/docs/Web/API/Client
-// Source: https://coryrylan.com/blog/simple-offline-page-with-service-workers
-// Original Source: https://googlechrome.github.io/samples/service-worker/custom-offline-page/
-// go here to delete a registered service worker, handy for debugging 
-// => chrome://inspect/#service-workers
-
 'use strict';
-
-// Incrementing CACHE_VERSION will start the install event and
-// force previously cached resources to be cached again.
-const CACHE_VERSION = '2';
+const CACHE_VERSION = '3';
 let CURRENT_CACHES = {
-  offline: 'offline-v' + CACHE_VERSION,
-  musicco: 'musicco-v' + CACHE_VERSION,
-  playlist: 'playlist'
+	offline: 'offline-v' + CACHE_VERSION,
+	musicco: 'musicco-v' + CACHE_VERSION,
+	playlist: 'playlist'
 };
 
 const OFFLINE_URL = 'offline.html';
 
 var urlsToCache = [
-  './'
+	'./'
 ];
 
 function firstWindowClient() {
-  return clients.matchAll({ type: 'window' }).then(function(windowClients) {
-    return windowClients.length ? windowClients[0] : Promise.reject("No clients");
-  });
+	return clients.matchAll({ type: 'window' }).then(function(windowClients) {
+		return windowClients.length ? windowClients[0] : Promise.reject("No clients");
+	});
 }
 
 self.addEventListener('install', event => {
@@ -45,35 +30,35 @@ self.addEventListener('install', event => {
 				});
 			})
 			})
-  );
-  event.waitUntil(
-    caches.open(CURRENT_CACHES.musicco)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+	);
+	event.waitUntil(
+		caches.open(CURRENT_CACHES.musicco)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
 self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
-    return CURRENT_CACHES[key];
-  });
+	// Delete all caches that aren't named in CURRENT_CACHES.
+	let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+		return CURRENT_CACHES[key];
+	});
 
-  event.waitUntil(
-    self.clients.claim().then(function() {caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (expectedCacheNames.indexOf(cacheName) === -1) {
-            // If this cache name isn't present in the array of "expected" cache names, then delete it.
-            console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-    })
-  );
+	event.waitUntil(
+		self.clients.claim().then(function() {caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(cacheName => {
+					if (expectedCacheNames.indexOf(cacheName) === -1) {
+						// If this cache name isn't present in the array of "expected" cache names, then delete it.
+						console.log('Deleting out of date cache:', cacheName);
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+		})
+	);
 });
 
 self.addEventListener('message', function(event){
@@ -99,12 +84,6 @@ self.addEventListener('message', function(event){
 						});
 					});
 			break;
-			case "update":
-				console.log("TODO: OK, I will inform you when the library update is completed");
-			break;
-			case "loaded":
-				console.log("TODO: don't do anything, you are out of date...");
-			break;
 			default:
 				console.log("I received this message but I don't know what to do with it!")
 				console.log(event.data);
@@ -112,24 +91,24 @@ self.addEventListener('message', function(event){
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' ||
-      (event.request.method === 'GET' &&
-       event.request.headers.get('accept').includes('text/html'))) {
-    console.log('Handling fetch event for', event.request.url);
-    event.respondWith(
-      fetch(event.request).catch(error => {
-        return caches.match(OFFLINE_URL);
-      })
-    );
-  }
+	if (event.request.mode === 'navigate' ||
+			(event.request.method === 'GET' &&
+			 event.request.headers.get('accept').includes('text/html'))) {
+		console.log('Handling fetch event for', event.request.url);
+		event.respondWith(
+			fetch(event.request).catch(error => {
+				return caches.match(OFFLINE_URL);
+			})
+		);
+	}
 });
 
 
 self.addEventListener('notificationclick', function(event) {  
-  self.clients.claim();
-  var notification = event.notification;
-  var actions = event.notification.actions;
-  var promise = Promise.resolve();
+	self.clients.claim();
+	var notification = event.notification;
+	var actions = event.notification.actions;
+	var promise = Promise.resolve();
 
 	if (event.action === '') {
 		promise = promise.then(function() { return firstWindowClient(); })
@@ -137,7 +116,7 @@ self.addEventListener('notificationclick', function(event) {
 	} else  {  
 		promise = promise.then(function() { return firstWindowClient(); })
 						.then(function(client) { client.postMessage(event.action) });
-  }  
+	}
 
 	event.waitUntil(promise);
 
