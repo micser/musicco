f66b0cbf6e1c6facac1f519dd88d3156fe03eb70
micser <micser.micser@gmail.com>
2024-08-08 20:03:16 +0200

    Fix #329: Upgrade to jquery-ui 1.14.0

--- index.php
+++ index.php
@@ -6189,7 +6189,7 @@ function refreshdb($quiet) {
 						$aboutString.="<li>Allow filtering search results by artist, album or song</li>";
 						$aboutString.="<li>Upgrade to wavesurfer 7.7.12</li>";
 						$aboutString.="<li>Upgrade to font-awesome 6.6.0</li>";
-						$aboutString.="<li>Upgrade to jquery-ui 1.13.3</li>";
+						$aboutString.="<li>Upgrade to jquery-ui 1.14.0</li>";
 						$aboutString.="<li>waveform style improvements</li>";
 					$aboutString.="</ul>";
 				$aboutString.="</li>";
